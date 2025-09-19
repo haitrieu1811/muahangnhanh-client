fb92f82347aac6eb1c7a384e0d7a6f9f4e3f1731
Tran Hai Trieu <haitrieu2524@gmail.com>
2025-09-19 17:26:45 +0700

    chore: thêm Tanstack Query vào dự án.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
+import TanstackQueryProvider from '@/providers/tanstack-query.provider'
 import './globals.css'
 
 const fontSans = Inter({
@@ -19,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`${fontSans.className} antialiased`}>{children}</body>
+      <body className={`${fontSans.className} antialiased`}>
+        <TanstackQueryProvider>{children}</TanstackQueryProvider>
+      </body>
     </html>
   )
 }
