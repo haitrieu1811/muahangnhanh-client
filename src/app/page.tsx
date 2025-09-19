db798dac11a26c51b44af77394c1f64fc979d9a5
Tran Hai Trieu <haitrieu2524@gmail.com>
2025-09-19 17:10:15 +0700

    chore: thêm Shadcn UI vào dự án.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
-  return <div>Trang chủ</div>
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  )
 }
